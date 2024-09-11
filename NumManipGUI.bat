c7ba6388be87cc14215f8a03d21b1acf7ea7c7fc
BryanRSummit <bryan@reddsummit.com>
2024-09-11 12:33:35 -0600

    changes small

--- NumManipGUI.bat
+++ NumManipGUI.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 "C:\Python312\python.exe" "C:\Users\%USERNAME%\Documents\Number Crusher\NumberManipulatorGUI.py"
+@REM pythonw.exe "C:\Users\%USERNAME%\Documents\Number Crusher\NumberManipulatorGUI.py"
 exit
 @REM @REM C:\Users\%USERNAME%\Documents\ReddSummitApps\NumberManipulator_GUI\build\exe.win-amd64-3.12\NumberManipulatorGUI.exe
