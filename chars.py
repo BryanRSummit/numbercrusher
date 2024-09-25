ea368f16f52de7c9b98c26628ca0755baa508446
BryanRSummit <bryan@reddsummit.com>
2024-09-25 11:48:25 -0600

    Fixed char removal, remove + and leading 1's

--- chars.py
+++ chars.py
@@ -17,7 +17,12 @@ def remove_chars(input_string):
     pattern = r"(\(\d+\)) (\d+)"
     input_string = re.sub(pattern, r"\1\2", input_string)
     # Then remove unneeded chars
-    removeThese = ["-", "(", ")", ".", "Click to dial", "disabled"]
+    removeThese = ["-", "(", ")", ".", "+", "Click to dial", "disabled"]
     for thing in removeThese:
         input_string = input_string.replace(thing, "")
+
+        # If the first element is '1' and the length is 11, remove the '1'
+    if input_string.startswith("1") and len(input_string) == 11:
+        input_string = input_string[1:]
+        
     return input_string
